426c3af6f50be09510ed4a0261b6128b6d3b53ce
millatur <minnatur7373@gmail.com>
2024-01-06 07:05:59 +0600

    edit some data

--- database/migrations/2024_01_05_173229_create_students_table.php
+++ database/migrations/2024_01_05_173229_create_students_table.php
@@ -17,7 +17,7 @@ return new class extends Migration
             $table->string('email');
             $table->integer('age');
             $table->integer('city');
-            $table->timestamps();
+
         });
     }
 
