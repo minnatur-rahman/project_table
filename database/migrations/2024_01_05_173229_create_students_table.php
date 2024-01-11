d754a361441c43bdbb11d98f0f4ad3ceb6221f71
millatur <minnatur7373@gmail.com>
2024-01-11 23:01:47 +0600

    edit some data

--- database/migrations/2024_01_05_173229_create_students_table.php
+++ database/migrations/2024_01_05_173229_create_students_table.php
@@ -13,10 +13,8 @@ return new class extends Migration
     {
         Schema::create('students', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email');
-            $table->integer('age');
-            $table->integer('city');
+            $table->string('name', 30);
+            $table->string('email', 40)->unique()->nullable();
             $table->timestamps();
 
         });
--- database/seeders/StudentSeeder.php
+++ database/seeders/StudentSeeder.php
@@ -13,39 +13,11 @@ class StudentSeeder extends Seeder
      */
     public function run(): void
     {
-        $students = [
-            [
+       student::created([
                 'id'=>1,
                 'name'=>'Minnatur Rahman',
                 'email'=>'minnatur@gmail.com',
-                'age'=>31,
-                'city'=>'Gaibandha'
-            ],
-            [
-                'id'=>2,
-                'name'=>'Mi Rahman',
-                'email'=>'mi@gmail.com',
-                'age'=>21,
-                'city'=>'Gai'
-            ],
-            [
-                'id'=>3,
-                'name'=>'Sojib Rahman',
-                'email'=>'sojib@gmail.com',
-                'age'=>30,
-                'city'=>'khulna'
-            ],
-            [
-                'id'=>2,
-                'name'=>'Mamun Rahman',
-                'email'=>'mamun@gmail.com',
-                'age'=>23,
-                'city'=>'Rangpur'
-            ]
-        ];
 
-
-        student::insert($students);
-
+       ]);
     }
 }
